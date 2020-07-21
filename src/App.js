573ad4e599c4f22eb944888e0f2a8f3fc89072ef
kew-dev <bitlabs.ksw@gmail.com>
2020-07-21 14:08:53 +0900

    백업

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+
+function Food({name}){
+
+return <h3>I love {name}</h3>;
+}
+
+const foodilike=[
+  {
+  name:"kimchi"
+  },
+  {
+    name:"kimc3hi"
+  },
+  {
+    name:"kim4chi"
+  }
+]
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return(
+   <div>
+     {foodilike.map(dish=>(
+       <Food name={dish.name}/>
+       ))}
+   </div>
   );
 }
 
